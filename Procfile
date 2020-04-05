7c68c08bd903c415dfd91050e3ec960b97776715
Mirrsa <mirrsa@mirrsa.mirrsa>
2020-04-05 12:38:33 +0530

    modfe

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn irisflower.wsgi
+web: gunicorn irisflower.wsgi --log-file -
--- irisflower/settings.py
+++ irisflower/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = os.environ['DEBUG']
 
 ALLOWED_HOSTS = []
 
